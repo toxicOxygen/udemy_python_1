3bf97701305c69f74886a6070e77e1de9457c5f1
Baffour <academicbaffour@gmail.com>
2021-02-26 05:49:45 +0000

    finished converter app

---
+++ distance_converter/main.py
@@ -0,0 +1,3 @@
+distanceInKilometer = float(input("Enter distance in km: "))
+distanceInMile = round(distanceInKilometer / 1.609344, 2)
+print(f"{distanceInKilometer} km ===> {distanceInMile} miles")
